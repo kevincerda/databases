7fae741d8b39a5c33ebf499468ac6eb6d101dcbf
Hack Reactor Student <student@hackreactor.com>
2018-05-18 20:12:05 -0700

    On our way to set up models.

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,12 @@
 var models = require('../models');
+var express = require('express');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) {
+      var sql = `select * from messages`;
+      res.send(200, /* either makes a erquest to model  */)
+    }, // a function which handles a get request for all messages
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
--- server/models/index.js
+++ server/models/index.js
@@ -1,9 +1,14 @@
 var db = require('../db');
+var controllers = require('../controllers')
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    get: function (req, res) {
+
+    }, // a function which produces all the messages
+    post: function (req, res) {
+      queryStr = `INSERT INTO MESSAGES (userID, message, roomID) VALUES (${req.username}, ${req.message}, ${req.roomname})`
+    } // a function which can be used to insert a message into the database
   },
 
   users: {

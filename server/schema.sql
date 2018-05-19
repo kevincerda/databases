9697f72306855bb99083d2a93d8025f9de439ed1
Hack Reactor Student <student@hackreactor.com>
2018-05-18 18:51:04 -0700

    Complete database scheme and build database for chatterbox.

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,36 @@
+DROP DATABASE chat;
+
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+CREATE TABLE users (
+  ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username varchar(255),
+  password varchar(255)
 );
 
-/* Create other tables and define schemas for them here! */
+CREATE TABLE rooms (
+  ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255),
+  userID int,
+  messageID int,
+  FOREIGN KEY (userID) REFERENCES users(ID)
+);
 
+CREATE TABLE messages (
+  ID int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  userID int,
+  message varchar (255),
+  roomID int,
+  FOREIGN KEY (roomID) REFERENCES rooms(ID),
+  FOREIGN KEY (userID) REFERENCES users(ID)
+);
 
+CREATE TABLE friendslist (
+  userID int PRIMARY KEY,
+  friendsID int,
+  FOREIGN KEY (userID) REFERENCES users(ID),
+  FOREIGN KEY (friendsID) REFERENCES users(ID)
 
-
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
-
+);
